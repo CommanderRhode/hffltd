3a2c2ddef56a38cb64e9855f04dc49d4e816aaa1
CommanderRhode <duncanfortescue@googlemail.com>
2014-11-19 23:48:14 +0000

    detecting index

--- hffltd.js
+++ hffltd.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
+  var responseMessage = 'HFF Ltd Web Server.\n' + 'request for: ' + req.url;
+  if (req.url=="/index") {responseMessage = "Wohoo, we hit Index.";}
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('HFF Ltd Web Server.\n' + 'request for: ' + req.url);
+  res.end(responseMessage);
 }).listen(3000, '0.0.0.0');
 console.log('Server running at http://0.0.0.0:3000/');
