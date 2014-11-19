5bdb143d2832bfc639f94cb57e0db8bdbe7b774c
CommanderRhode <duncanfortescue@googlemail.com>
2014-11-19 23:59:46 +0000

    looking for existing static content

--- hffltd.js
+++ hffltd.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
   var responseMessage = 'HFF Ltd Web Server.\n' + 'request for: ' + req.url;
-  if (req.url=="/index") {responseMessage = "Wohoo, we hit Index.";}
+  if (fs.existsSync('static' + req.url + '.html'))
+    //lets get that file
+    {responseMessage += ' EXISTS!'; }
+  else
+    //my 404 message
+    {responseMessage += ' is not here'; }
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end(responseMessage);
 }).listen(3000, '0.0.0.0');
