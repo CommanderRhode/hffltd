3ffe48acf1a66d9cb0e54bb9596a7a075fa462bb
CommanderRhode <duncanfortescue@googlemail.com>
2014-11-24 22:45:35 +0000

    adding Jasmine suite

---
+++ spec/requestActivatorSpec.js
@@ -0,0 +1,7 @@
+var requestActivator = require('../server/requestActivator.js');
+
+describe("Making sure requestActivator behaves", function() {
+  it("when a known file is passed in", function() {
+    expect(true).toBe(true);
+  });
+});
